b4624d02eea64d877ac211783a258614458384cb
needgreen <need.green.jei@gmail.com>
2026-02-05 18:19:00 +0900

    feat: 마이페이지 좋아요 목록 스크롤 기능 구현 및 대시보드 컴포넌트 구조 리팩토링
    - 대시보드 헤더 영역 컴포넌트 분리 (dashboard-header/)
    - 좋아요, 플레이리스트, 내가만든숏츠 총 갯수 표시
    - Shorts 관련 컴포넌트 네이밍 개선 (Liked*, ShortsPlaylist*)
    - 좋아요 숏츠 목록 가로 스크롤 기능 구현(버튼 스와이프)
    - 더미 데이터 정리 및 db.json으로 통합

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -1,5 +1,4 @@
 import PlaylistSection from '@/features/home/playlist/PlaylistSection'
-import { playlistGroup } from '@/dummy/data'
 import { getShortPopular } from '@/services/shorts/getShortPopular'
 import { categoryApi } from '@/services/category/category.service'
 import CategoryShortsSection from '@/features/home/categories/CategoryShortsSection'
@@ -8,11 +7,13 @@ import { ShortsBase } from '@/types/shorts/shorts'
 
 export default async function Page() {
   // api 호출 (초기 로드: 전체 숏츠)
-  const [popularShorts, categoryShortsResponse, categoriesResponse] = await Promise.all([
-    getShortPopular(),
-    categoryApi.getAllShorts({ sort: 'createdAt,desc' }), // 전체 숏츠 목록(최신순 기본값)
-    categoryApi.getAllCategoryId(), // 카테고리 항목 목록
-  ])
+  const [popularShorts, categoryShortsResponse, categoriesResponse, playlistResponse] =
+    await Promise.all([
+      getShortPopular(),
+      categoryApi.getAllShorts({ sort: 'createdAt,desc' }), // 전체 숏츠 목록(최신순 기본값)
+      categoryApi.getAllCategoryId(), // 카테고리 항목 목록
+      fetch('http://localhost:4000/playlists').then((res) => res.json()), // Mock server >> api 연동 필요
+    ])
 
   // 인기 숏츠 (캐러셀용)
   const shortsList: ShortsBase[] = popularShorts?.data?.content ?? []
@@ -31,7 +32,7 @@ export default async function Page() {
     <div className="min-h-screen bg-white">
       <ShortsCarousel data={shortsList} />
 
-      <PlaylistSection items={playlistGroup} />
+      <PlaylistSection items={playlistResponse} />
 
       <CategoryShortsSection initialShortsData={initialShortsData} categories={categories} />
     </div>
